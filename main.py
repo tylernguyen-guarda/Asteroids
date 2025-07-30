4923a0b7c591c138f1124a03c5af089129a4146d
tylerguarda <tylernguarda@gmail.com>
2025-07-30 15:07:51 -0400

    CH2 L7 complete

--- main.py
+++ main.py
@@ -8,21 +8,42 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    # Create group to manage objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # assign player object to both groups
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-        player.draw(screen)
-        pygame.display.flip()
+
+        # calculate time difference between frames
         tick_time = clock.tick(60)
+        print(tick_time)
         dt = tick_time / 1000
+
+        # update game state
+        player.update(dt)
+
+        # clear screen
+        screen.fill("black")
+
+        # draw everything to screen
+        player.draw(screen)
+
+        # display frame
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,22 +1,44 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.containers = []
 
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = self.position + forward * self.radius  # type: ignore
+        b = self.position - forward * self.radius - right  # type: ignore
+        c = self.position - forward * self.radius + right  # type: ignore
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)  # type: ignore
+
+    def rotate(self, delta_time):
+        self.rotation += delta_time * PLAYER_TURN_SPEED
+
+    def move(self, delta_time):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * delta_time
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
